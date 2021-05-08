73840673bd124a4c4a878b1974cacefa2128fd09
sargy <lukina_sargy@mail.ru>
2021-05-09 00:15:30 +0300

    fix

--- 004-migrate/library/src/index.ts
+++ 004-migrate/library/src/index.ts
@@ -31,11 +31,9 @@ const UserDB = process.env.DB_USERNAME || 'root';
 const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
 const NameDB = process.env.DB_NAME || 'books'
 const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
-const mongodb = `mongodb://${UserDB}:${PasswordDB}@${HostDb}/${NameDB}`;
 
 async function start () {
     try {
-        // await mongoose.connect(mongodb)
         await mongoose.connect(HostDb, {
             user: UserDB,
             pass: PasswordDB,
