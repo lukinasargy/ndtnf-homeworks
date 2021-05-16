e9f6966067ad266f11c2fba7de1e254310fe2f9a
sargy <lukina_sargy@mail.ru>
2021-05-16 22:21:57 +0300

    init

--- 004-migrate/library/src/middleware/error.ts
+++
@@ -1,5 +0,0 @@
-export const errorMiddleware = (req:any, res:any) => {
-    res.render("error/404", {
-        title: "404 | страница не найдена",
-    });
-};
--- 004-migrate/library/src/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/login", (req:any, res:any) => {
-    res.json({ id: 1, mail: "test@mail.ru" });
-    res.status(201)
-});
-export const userRouter = router;
