5b7a57549189125d0040d9e5aad13ced2f5f29dc
sargy <lukina_sargy@mail.ru>
2021-07-04 16:31:29 +0300

    init

--- 008-nestjs-db/library/src/dto/create-book.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateBookDto {
-    title: string;
-    description: string;
-    authors: string;
-    favorite: boolean;
-    fileCover: string;
-    fileName: string;
-    fileBook: string;
-  }
--- 008-nestjs-db/library/src/dto/update-book.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UpdateBookDto {
-    title: string;
-    description: string;
-    authors: string;
-    favorite: boolean;
-    fileCover: string;
-    fileName: string;
-    fileBook: string;
-  }
